a53f8b760a6914c36c6f231234562d226dbc16f1
Seaver Milnor <milnor@users.noreply.github.com>
2022-04-30 23:31:10 +0200

    solid first draft

--- coursera1_intro/stats.c
+++ coursera1_intro/stats.c
@@ -21,6 +21,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>     // Is this against the rules? I want calloc/free.
 #include "stats.h"
 
 /* Size of the Data Set */
@@ -35,23 +36,41 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
+
   /* Statistics and Printing Functions Go Here */
-    
+    print_statistics(test, SIZE);
+    printf("UNSORTED:\n");
     print_array(test, SIZE);
-    printf("min = %d\n", find_minimum(test, SIZE));
-    printf("max = %d\n", find_maximum(test, SIZE));
-    printf("mean = %d\n", find_mean(test, SIZE));
+    printf("SORTED:\n");
+    if (0 != sort_array(test, SIZE)) 
+    {
+        fprintf(stderr, "[-] sort failed\n");
+    }
+    else
+    {
+        print_array(test, SIZE);
+    }
 }
 
 /* Add other Implementation File Code Here */
+void print_statistics(unsigned char * array, size_t length)
+{
+    printf("Minimum = %d\n", find_minimum(array, length));
+    printf("Maximum = %d\n", find_maximum(array, length));
+    printf("Mean    = %d\n", find_mean(array, length));
+    printf("Median  = %d\n", find_median(array, length));
+}
 
-// TODO: wrap to max of 16 per line
 void print_array(unsigned char * array, size_t length)
 {
     printf("Array of size %zu:\n", length);
     for (size_t i = 0; i < length; i++)
     {
         printf("%d ", array[i]);
+        if (0 == (length % 16)) // line wrap
+        {
+            printf("\n");
+        }
     }
     printf("\n");
 }
@@ -92,3 +111,71 @@ unsigned char find_maximum(unsigned char * array, size_t length)
     }
     return biggest;
 }
+
+// TODO: optimize me! this is effectively bubblesort from Schaffer, p. 236
+int sort_array(unsigned char * array, size_t length)
+{
+    if (NULL == array)
+    {
+        return 1;
+    }
+
+    for (size_t i = 0; i < (length - 1); i++)
+    {
+        for (size_t j = (length - 1); j > i; j--)
+        {
+            if (array[j] > array[(j-1)])
+            {
+                /* swap! */
+                unsigned char temp = array[j];
+                array[j] = array[(j-1)];
+                array[(j-1)] = temp;
+            }
+        }
+    }
+    return 0;
+}
+
+unsigned char find_median(unsigned char * array, size_t length)
+{
+    /* Probably cleaner to copy the array than to mess with the
+     * original array*/
+    unsigned char median = 0;
+    unsigned char * copy = calloc(length, sizeof(unsigned char));
+    if (NULL == copy)
+    {
+        fprintf(stderr, "[-] calloc failed\n");
+        goto fail;
+    }
+    for (size_t i = 0; i < length; i++)
+    {
+        copy[i] = array[i];
+    }
+
+    if (0 != sort_array(copy, length))
+    {
+        goto fail;
+    }
+
+    if (1 == (length % 2))
+    {
+        // odd number of elements
+        median = array[(length / 2) +1];
+    }
+    else
+    {
+        // even number of elements
+        size_t middle = length / 2;
+        unsigned int sum = array[middle] + array [middle - 1];
+        median = sum / 2;
+    }
+    free(copy);
+
+    return median;
+
+fail:
+    /* no error return value available due to API limitation */
+    fprintf(stderr, "[-] median failed\n");
+    return (unsigned char) -1;    
+}
+
