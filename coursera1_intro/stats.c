6d43bd4104ca0098845d383ab794924a2cf0b9ca
Seaver Milnor <milnor@users.noreply.github.com>
2022-04-30 19:19:23 +0200

    most trivial ones implemented

--- coursera1_intro/stats.c
+++ coursera1_intro/stats.c
@@ -9,17 +9,16 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief Display stats of a char array.
  *
- * <Add Extended Description Here>
+ * Given an array of unsigned chars, print out the minimum, maximum, mean,
+ *  and median.
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Milnor
+ * @date April 30, 2022
  *
  */
-
-
 
 #include <stdio.h>
 #include "stats.h"
@@ -37,7 +36,59 @@ void main() {
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
-
+    
+    print_array(test, SIZE);
+    printf("min = %d\n", find_minimum(test, SIZE));
+    printf("max = %d\n", find_maximum(test, SIZE));
+    printf("mean = %d\n", find_mean(test, SIZE));
 }
 
 /* Add other Implementation File Code Here */
+
+// TODO: wrap to max of 16 per line
+void print_array(unsigned char * array, size_t length)
+{
+    printf("Array of size %zu:\n", length);
+    for (size_t i = 0; i < length; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+/* Assumes integer division is desired behavior */
+unsigned char find_mean(unsigned char * array, size_t length)
+{
+    unsigned int sum = 0;
+    for (size_t i = 0; i < length; i++)
+    {
+        sum += array[i];
+    }
+    return sum / length;
+}
+
+unsigned char find_minimum(unsigned char * array, size_t length)
+{
+    unsigned char smallest = array[0];
+    for (size_t i = 1; i < length; i++)
+    {
+        if (array[i] < smallest)
+        {
+            smallest = array[i];
+        }
+    }
+    return smallest;
+}
+
+unsigned char find_maximum(unsigned char * array, size_t length)
+{
+    unsigned char biggest = array[0];
+    for (size_t i = 1; i < length; i++)
+    {
+        if (array[i] > biggest)
+        {
+            biggest = array[i];
+        }
+    }
+    return biggest;
+}
